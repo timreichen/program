ac3b99175eba450d716ccaf926ac768be41c449b
Tim Reichen <tim.reichen@gmail.com>
2020-08-17 05:31:05 +0200

    add nested command , restruct

--- _helpers.ts
+++ _helpers.ts
@@ -1,153 +1,185 @@
-export interface Argument {
-  name: string
-  optional?: boolean
-  multiple?: boolean
-}
+import { Argument, Option, Command } from "./command.ts";
 
-export interface Option {
-  name: string
-  description: string
-  alias?: string,
-  boolean?: boolean
-  args?: Argument[]
-}
+type Collection = string[][];
 
-export interface Command {
-  name: string
-  description: string
-  options?: Option[]
-  args?: Argument[]
-}
+const padding = 4;
 
-export interface Program extends Command {
-  version?: string | number
-  subcommands?: Command[]
-}
-
-type Collection = string[][]
-
-const padding = 4
-
-function decoreateArgument({ name, optional, multiple }: Argument) {
-  let string = optional ? `[${name}]` : `<${name}>`
-  if (multiple) { string += "..." }
-  return string
+function decoreateArgument(
+  { name, optional, description, multiple }: Argument,
+) {
+  let string = optional ? `[${name}]` : `<${name}>`;
+  if (multiple) string += "...";
+  return string;
 }
 function stringifyCollection(collection: Collection, spaces: number[]) {
-  let string = ""
+  let string = "";
 
   for (const values of collection) {
-
     for (const [index, value] of values.entries()) {
-      const lines = value.split(`\n`)
-      string += " ".repeat(padding)
-      string += lines.shift()!.padEnd(spaces[index])
+      const lines = value.split(`\n`);
+      string += " ".repeat(padding);
+      string += lines.shift()!.padEnd(spaces[index]);
       for (const line of lines) {
-        string += "\n"
-        string += " ".repeat(padding * 2 + spaces[0])
-        string += line
+        string += "\n";
+        string += " ".repeat(padding * 2 + spaces[0]);
+        string += line;
       }
     }
-    string += "\n"
+    string += "\n";
   }
-  return string
+  return string;
 }
 function stringifyPart(name: string, lines: string[]) {
-  let string = ""
-  string += `${name}`
-  string += `\n`
+  let string = "";
+  string += `${name}`;
+  string += `\n`;
   for (const line of lines) {
-    string += `${" ".repeat(padding)}${line}`
-    string += `\n`
+    string += `${" ".repeat(padding)}${line}`;
+    string += `\n`;
   }
-  return string
+  return string;
 }
 function stringifyArguments(args: Argument[] = []) {
-  return args.map(arg => decoreateArgument(arg)).join(" ")
+  return args.map((arg) => decoreateArgument(arg)).join(" ");
 }
 function createSpaces(...collections: Collection[]) {
-  const spaces: number[] = []
+  const spaces: number[] = [];
   for (const collection of collections) {
     for (const strings of collection) {
       for (const [index, string] of strings.entries()) {
-        const longestLineLength = Math.max(...string.split(`\n`).map(line => line.length))
-        spaces[index] = Math.max(spaces[index] || 0, longestLineLength)
+        const longestLineLength = Math.max(
+          ...string.split(`\n`).map((line) => line.length),
+        );
+        spaces[index] = Math.max(spaces[index] || 0, longestLineLength);
       }
     }
   }
-  return spaces
+  return spaces;
 }
-function stringifySection(name: string, collection: Collection, spaces?: number[]) {
-  spaces = spaces || createSpaces(collection)
-  let string = ""
-  string += name
-  string += `\n`
-  string += stringifyCollection(collection, spaces)
-  return string
+function stringifySection(
+  name: string,
+  collection: Collection,
+  spaces?: number[],
+) {
+  spaces = spaces || createSpaces(collection);
+  let string = "";
+  string += name;
+  string += `\n`;
+  string += stringifyCollection(collection, spaces);
+  return string;
 }
-function stringifyUsage({ name, options = [], args = [], subcommands = [] }: { name: string, options?: Option[], args?: Argument[], subcommands?: Command[] }) {
-  const optionsString = options.length ? "[OPTIONS]" : ""
-  const subcommandsString = "[SUBCOMMAND]"
-  return stringifyPart("USAGE:", [`${name} ${optionsString} ${subcommands.length ? subcommandsString : stringifyArguments(args)}`])
+function stringifyUsage(
+  { name, options = [], args = [], subcommands = [] }: {
+    name: string;
+    options?: Option[];
+    args?: Argument[];
+    subcommands?: Command[];
+  },
+) {
+  const optionsString = options.length ? "[OPTIONS]" : "";
+  const subcommandsString = "[SUBCOMMAND]";
+  return stringifyPart(
+    "USAGE:",
+    [`${name} ${optionsString} ${
+      subcommands.length ? subcommandsString : stringifyArguments(args)
+    }`],
+  );
 }
 
-export function createHelp({ title, name, usageName, version, description, options = [], args = [], subcommands = [] }: { title?: string, usageName?: string, name: string, description: string, version?: string, options: Option[], args: Argument[], subcommands?: Command[] }) {
-  const subcommandsCollection = subcommands.map((subcommand: any) => [subcommand.name, subcommand.description])
-  const optionsCollection = options.map((option: any) => {
-    const aliasFlag = option.alias ? `-${option.alias}, ` : " ".repeat(4)
-    const flag = `--${option.name}`
-    const args = option.args ? option.args.map((arg: Argument) => decoreateArgument(arg)).join(" ") : ""
-    return [`${aliasFlag}${flag}${args ? ` ${args}` : ""}`, option.description]
-  })
-  const argsCollection = args.map((arg: any) => [decoreateArgument(arg), ""])
+export function createHelp(
+  {
+    title,
+    name,
+    usageName,
+    version,
+    description,
+    options = [],
+    args = [],
+    subcommands = [],
+  }: {
+    title?: string;
+    usageName?: string;
+    name: string;
+    description: string;
+    version?: string;
+    options: Option[];
+    args: Argument[];
+    subcommands?: Command[];
+  },
+) {
+  const subcommandsCollection = subcommands.map((
+    subcommand: Command,
+  ) => [subcommand.name, subcommand.description]);
+  const optionsCollection = options.map((option: Option) => {
+    const aliasFlag = option.alias ? `-${option.alias}, ` : " ".repeat(4);
+    const flag = `--${option.name}`;
+    const args = option.args
+      ? option.args.map((arg: Argument) => decoreateArgument(arg)).join(" ")
+      : "";
+    return [`${aliasFlag}${flag}${args ? ` ${args}` : ""}`, option.description];
+  });
+  const argsCollection = args.map((
+    arg: Argument,
+  ) => [decoreateArgument(arg), arg.description || ""]) as Collection;
 
-  const spaces = createSpaces(subcommandsCollection, optionsCollection)
+  const spaces = createSpaces(subcommandsCollection, optionsCollection);
 
-  let string = ""
-  string += title || name
-  string += version ? ` ${version}` : ""
-  string += `\n`
-  string += description || ""
-  string += `\n`
-  string += `\n`
-  string += stringifyUsage({ name: usageName || name, options, args, subcommands })
+  let string = "";
+  string += title || name;
+  string += version ? ` ${version}` : "";
+  string += `\n`;
+  string += description || "";
+  string += `\n`;
+  string += `\n`;
+
+  string += stringifyUsage(
+    { name: usageName || name, options, args, subcommands },
+  );
   if (optionsCollection.length) {
-    string += `\n`
-    string += stringifySection("OPTIONS:", optionsCollection, spaces)
+    string += `\n`;
+    string += stringifySection("OPTIONS:", optionsCollection, spaces);
   }
   if (subcommandsCollection.length) {
-    string += `\n`
-    string += stringifySection("SUBCOMMANDS:", subcommandsCollection, spaces)
+    string += `\n`;
+    string += stringifySection("SUBCOMMANDS:", subcommandsCollection, spaces);
   }
   if (argsCollection.length) {
-    string += `\n`
-    string += stringifySection("ARGS:", argsCollection)
+    string += `\n`;
+    string += stringifySection("ARGS:", argsCollection);
   }
-  return string
-
+  return string;
 }
 
-export function createError({ name, requiredArguments, options, args, subcommands }: { name: string, requiredArguments: Argument[], options: Option[], args: Argument[], subcommands?: Command[] }) {
-  let string = ""
-  string += "error: The following required arguments were not provided:"
-  string += `\n`
+export function createError(
+  { name, requiredArguments, options, args, subcommands }: {
+    name: string;
+    requiredArguments: Argument[];
+    options: Option[];
+    args: Argument[];
+    subcommands?: Command[];
+  },
+) {
+  let string = "";
+  string += "error: The following required arguments were not provided:";
+  string += `\n`;
   for (const argument of requiredArguments) {
-    string += `${" ".repeat(padding)}${decoreateArgument(argument)}`
-    string += `\n`
+    string += `${" ".repeat(padding)}${decoreateArgument(argument)}`;
+    string += `\n`;
   }
-  string += `\n`
-  string += stringifyUsage({ name, options, args, subcommands })
-  string += `\n`
-  string += `For more information try --help`
-  return string
+  string += `\n`;
+  string += stringifyUsage({ name, options, args, subcommands });
+  string += `\n`;
+  string += `For more information try --help`;
+  return string;
 }
 
 export function invalidSubcommandError(name: string, subcommands: string[]) {
-  let string = `error: The subcommand '${name}' wasn't recognized`
-  return string
+  let string = `error: The subcommand '${name}' wasn't recognized`;
+  return string;
 }
 
 export function invalidArgumentError(name: string) {
-  let string = `error: Found argument '${name}' which wasn't expected, or isn't valid in this context`
-  return string
+  let string =
+    `error: Found argument '${name}' which wasn't expected, or isn't valid in this context`;
+  return string;
 }
--- example.ts
+++ example.ts
@@ -1,24 +1,65 @@
 // import { Program } from "https://raw.githubusercontent.com/timreichen/program/master/mod.ts"
-import { Program } from "./program.ts"
+import { Program } from "./program.ts";
+import { invalidSubcommandError } from "./_helpers.ts";
 
-function log(args: { [option: string]: any }) {
-  if (args.quiet) { return }
-  console.log(args)
+function log(args: { [option: string]: string | number }) {
+  if (args.quiet) return;
+  console.log(args);
 }
 
-const program = new Program({ name: "logger", description: "logs parsed arguments and options", version: "1.0.1", fn: log })
+const program = new Program(
+  {
+    name: "logger",
+    description: "logs parsed arguments and options",
+    version: "1.0.1",
+    fn: log,
+  },
+);
 
 program
   .command({
     name: "log",
     description: "logs parsed arguments and options",
-    fn: log
+    fn: log,
   })
   .option({
     name: "quiet",
     alias: "q",
     description: "Suppress diagnostic output",
   })
-  .argument({ name: "argument", multiple: true, optional: true })
+  .argument({ name: "argument", multiple: true, optional: true });
 
-program.parse(Deno.args)
+function h(args: string[]) {
+  if (!args.length) {
+    return program.help();
+  }
+  for (const cmd of args) {
+    if (!program.commands[cmd]) {
+      return console.log(
+        invalidSubcommandError(cmd, Object.keys(program.commands)),
+      );
+    }
+  }
+  const cmd = args[0];
+  const command = program.commands[cmd];
+  return command.help();
+}
+
+program
+  .command(
+    {
+      name: "help",
+      description: "Prints this message or the help of the given subcommand(s)",
+      fn: h,
+    },
+  )
+  .argument(
+    {
+      name: "subcommands",
+      description: "The subcommand whose help message to display",
+      optional: true,
+      multiple: true,
+    },
+  );
+
+program.parse(Deno.args);
